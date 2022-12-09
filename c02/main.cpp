8519b7516838fbd6b8ad880ca38a7ea3d2f31325
CGLee <cglee@snowpipe.co.kr>
2022-12-09 17:02:06 +0900

    Refactor:  c02

--- c02/main.cpp
+++ c02/main.cpp
@@ -3,33 +3,32 @@ using namespace std;
 
 #define MX 10001
 
-int main(void){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
 
-    int n, k, nv;
-    int dp[MX] = {0,};
+  int n, k, nv;
+  int i, j;
 
-    cin >> n >> k;
+  cin >> n >> k;
 
-    for (int i=1; i<=k; i++) {
-        dp[i] = MX;
+  int dp[MX] = {0,};
+  for (i = 1; i <= k; i++) {
+    dp[i] = MX;
+  }
+
+  for (i = 1; i <= n; i++) {
+    cin >> nv;
+    for (j = nv; j <= k; j++) {
+      dp[j] = min(dp[j], dp[j - nv] + 1);
     }
+  }
 
-    for (int i=1; i<=n; i++) {
-        cin >> nv;
-
-        for(int j=nv; j<=k; j++) {
-            dp[j] = min( dp[j], dp[j-nv]+1);
-        }
-    }
-
-    if (dp[k] == MX) {
-        cout << -1 << endl;
-    } else {
-        cout << dp[k] << endl;
-    }
-
+  if (dp[k] == MX) {
+    cout << -1 << endl;
+  } else {
+    cout << dp[k] << endl;
+  }
 
   return 0;
 }
