55728cccb4cc81564178f080a2a57ef72fe63d8f
CGLee <cglee@snowpipe.co.kr>
2022-12-10 16:56:54 +0900

    Feat: Add c03

---
+++ c03/main.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MX 10001
+
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int N, TK, K, P;
+  int i, j;
+
+  int dp[MX] = {
+      0,
+  };
+
+  cin >> N >> TK;
+
+  for (i = 1; i <= N; i++) {
+    cin >> K >> P;
+
+    for (j = K; j <= TK; j++) {
+      dp[j] = max(dp[j - K] + P, dp[j]);
+    }
+  }
+
+  if (dp[TK] == 0) {
+    cout << -1 << endl;
+  } else {
+    cout << dp[TK] << endl;
+  }
+
+  return 0;
+}
