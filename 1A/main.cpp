bee65bacc40f2941bd4eb33965bfd8712190dc82
CGLee <cglee@snowpipe.co.kr>
2023-05-26 01:04:35 +0900

    Feat: first commit

---
+++ 1A/main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string name = "chulgil";
+
+int main() {
+
+    // 앞에서 부터 3개 출력
+    cout << name.substr(0, 3) << '\n';
+
+    // 거꾸로
+    reverse(name.begin(), name.end());
+    cout << name << '\n';
+
+    // lee 추가하기
+    name += "lee";
+    cout << name << '\n';
+
+    return 0;
+}
